aa33baae5a48db7c87d3f9b60d161d3c5b0e5496
AssiaChemlali <chemlali.assia@gmail.com>
2025-02-14 11:38:50 +0100

    fix setup

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Navbar = () => {
+const ThemeSettings = () => {
   return (
     <div>
-      navbar
+      
     </div>
   )
 }
 
-export default Navbar
+export default ThemeSettings
