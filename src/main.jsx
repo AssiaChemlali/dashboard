9ac3b054e298ff6bfedf6cadd63ada4e15972be3
AssiaChemlali <chemlali.assia@gmail.com>
2025-02-21 10:30:33 +0100

    add context

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { registerLicense } from '@syncfusion/ej2-base';
-
+import { ContextProvider } from './contexts/ContextProvider.js';
 registerLicense('Ngo9BigBOggjHTQxAR8/V1NMaF1cWGhKYVJzWmFZfVtgcl9CaVZVRmYuP1ZhSXxWdkdiXX9acHNQR2JVUEE=');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+
   </StrictMode>,
 )
