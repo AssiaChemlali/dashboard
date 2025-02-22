c43db8ad7d7b0f3824221e5f7d46d532fe9668ba
AssiaChemlali <chemlali.assia@gmail.com>
2025-02-22 12:38:57 +0100

    code ecommerce page

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
               <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
                 <Navbar/>
               </div>
-            </div>
+           
    
             <div>
               <Routes>
@@ -66,7 +66,7 @@ function App() {
               </Routes>
             </div>
 
-
+            </div>
       </div>
     </BrowserRouter>
    </div>
