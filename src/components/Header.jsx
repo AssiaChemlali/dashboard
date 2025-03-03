9dbf3d8fa4d76c5155faebf68966af03f795352b
AssiaChemlali <chemlali.assia@gmail.com>
2025-03-03 12:36:12 +0100

    add employees page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = ({category,title}) => {
   return (
       
-  <div className='mt-10'>
+  <div className='my-10'>
     <p className='text-gray-400 '>{category}</p>
     <p className='text-3xl font-extrabold mt-2 text-slate-900 tracking-tight'>{title}</p>
   </div>
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { employeesData,employeesGrid } from '../data/dummy'
+import {GridComponent,ColumnsDirective, ColumnDirective,Page,Search, Inject,Toolbar} from '@syncfusion/ej2-react-grids'
 
+import {Header} from '../components'
 const Employees = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='bg-white p-2 md:m-10 md:p-10 rounded-3xl m-2'>
+    <Header 
+    category="Page" 
+    title="Employees"/>
+
+    <GridComponent 
+    id="gridcomp"
+    dataSource={employeesData}
+    allowPaging
+   toolbar={['Search']}
+   width="auto"
+    >
+      <ColumnsDirective>
+        {employeesGrid.map((item,index)=> <ColumnDirective key={index} {...item}/>
+        )}
+      </ColumnsDirective>
+      <Inject services={[Page,Toolbar,Search]}/>
+    </GridComponent>
+ 
+
+
+   
+  </div>
   )
 }
 
