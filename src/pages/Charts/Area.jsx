d7958ddd299b7fa14444e61fa15669e2a0c6d2bd
AssiaChemlali <chemlali.assia@gmail.com>
2025-03-05 11:17:44 +0100

    add area chat

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { ChartComponent,SeriesDirective,SeriesCollectionDirective,Inject,DateTime,Legend, SplineAreaSeries } from '@syncfusion/ej2-react-charts'
 
+import { areaCustomSeries,areaPrimaryXAxis,areaPrimaryYAxis } from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
+
+import {Header} from'../../components'
 const Area = () => {
-  return (
-    <div>
-      
-    </div>
+
+      const {currentMode}=useStateContext()
+      return (
+
+        <div className=''>
+           <Header category="Area" title="Inflation Rate in Percentage"/>
+          <ChartComponent
+          id="area-chart"
+          height='420px'
+          primaryXAxis={areaPrimaryXAxis}
+          primaryYAxis={areaPrimaryYAxis}
+          chartArea={{border:{width:0}}}
+          background={currentMode==="Dark" ? "#33373E":"#fff"}
+          legendSettings={{ background: 'white' }}
+          >
+          <Inject services={[SplineAreaSeries,DateTime,Legend]}/>
+          <SeriesCollectionDirective>
+            {areaCustomSeries.map((item,index)=>
+              <SeriesDirective key={index} {...item}/>
+            )}
+          </SeriesCollectionDirective>
+          </ChartComponent>
+        </div>
   )
 }
 
--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { ChartComponent,SeriesDirective,SeriesCollectionDirective,Inject,DateTime,Legend, SplineAreaSeries } from '@syncfusion/ej2-react-charts'
 
+import { areaCustomSeries,areaPrimaryXAxis,areaPrimaryYAxis } from '../../data/dummy'
+import { useStateContext } from '../../contexts/ContextProvider'
+
+import {Header} from'../../components'
 const Bar = () => {
+  const {currentMode}=useStateContext()
   return (
-    <div>
-      
-    </div>
+    <div className=''>
+    <Header category="Chart" title="Bar"/>
+
+
+    <ChartComponent
+    id="bar-chart"
+    height='420px'
+    primaryXAxis={areaPrimaryXAxis}
+    primaryYAxis={areaPrimaryYAxis}
+    chartArea={{border:{width:0}}}
+    background={currentMode==="Dark" ? "#33373E":"#fff"}
+    legendSettings={{ background: 'white' }}
+    >
+    <Inject services={[SplineAreaSeries,DateTime,Legend]}/>
+    <SeriesCollectionDirective>
+      {areaCustomSeries.map((item,index)=>
+        <SeriesDirective key={index} {...item}/>
+      )}
+    </SeriesCollectionDirective>
+    </ChartComponent>
+
+
+ </div>
   )
 }
 
